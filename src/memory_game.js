3537616724f20bc9fb2a1ed480afdcd625e58076
Jeanette <jbrasepae@gmail.com>
2020-07-08 16:51:44 +0200

    adding function

---
+++ src/memory_game.js
@@ -0,0 +1,9 @@
+const cards = document.querySelectorAll('.memory-card');
+
+function displayCard(){
+this.classiclist.toggle('display');
+
+// for (var i = 0; i < cards.length; i++){
+//     cards[i].addEventListener("click", displayCard);
+//  };
+}
