297d67dd0eccb3ebb5c816f3bc16479b120099e9
Jeanette <jbrasepae@gmail.com>
2020-07-22 22:43:41 +0200

    third commit

--- src/memory_game.js
+++ src/memory_game.js
@@ -1,9 +1,57 @@
-const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll('.card');
 
-function displayCard(){
-this.classiclist.toggle('display');
+let flippedCard = true;
+let firstCard;
+let secondCard;
 
-// for (var i = 0; i < cards.length; i++){
-//     cards[i].addEventListener("click", displayCard);
-//  };
+ function flipCard(){
+    this.classList.toggle('turn');
+
+    if(flippedCard){
+        flippedCard = false;
+        firstCard = this;
+
+    }
+
+    else{
+        flippedCard = true;
+        secondCard = this;
+
+        matching();    
+    }
 }
+
+function matching(){
+    if(firstCard.dataset.name !== secondCard.dataset.name){
+        flipBackCards();
+    }
+
+    else{ 
+        freezeCards();
+    }
+
+}
+
+function freezeCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function flipBackCards(){
+    setTimeout(() => {
+        firstCard.classList.remove('turn');
+        secondCard.classList.remove('turn');
+    }, 900);
+}
+
+
+
+(function shuffle(){
+    cards.forEach(card => {
+        let randPosition = Math.floor(Math.random() * 12);
+        card.style.order = randPosition;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard))
+
