95f0d8506938130b45f6a87f446f7fa839e840bd
Jeanette <jbrasepae@gmail.com>
2020-08-04 23:30:32 +0200

    added test

--- spec/memory_game_spec.js
+++ spec/memory_game_spec.js
@@ -1,3 +1,22 @@
-const html = require('../src/script.js')
-	const jsdom = require('jsdom')
-	const {JSDOM} = jsdom;
+const {JSDOM} = require('jsdom');
+
+	describe('memory_game.html', function(){
+		var browser;
+
+		beforeEach (function(done){
+			JSDOM.fromFile('./src/memory_game.html',{
+
+			}).then(function(res){
+				browser = res;
+				done();
+			})
+
+		})
+		afterEach(function(){
+			browser.window.close();
+		})
+		it('should have a <h1> element', function(){
+			var h1 = browser.window.document.querySelector('h1');
+			expect(h1).not.toBe(null);
+		})
+	})
