88b3557abd735800fbc853b779f10d589c0638ac
Jeanette <jbrasepae@gmail.com>
2020-08-06 17:49:11 +0200

    added other specs

--- spec/memory_game_spec.js
+++ spec/memory_game_spec.js
@@ -10,13 +10,23 @@ const {JSDOM} = require('jsdom');
 				browser = res;
 				done();
 			})
-
+			
 		})
 		afterEach(function(){
 			browser.window.close();
 		})
 		it('should have a <h1> element', function(){
 			var h1 = browser.window.document.querySelector('h1');
-			expect(h1).not.toBe(null);
+			expect(h1).not.toBeNull();
 		})
-	})
+		it ('should have div elements', function(){
+			var div = browser.window.document.querySelector('div');
+			expect(div).not.toBe(null);
+		})
+		it('game must have 12 cards', function()  {
+            const cards = browser.window.document.querySelectorAll('.card');
+			expect(cards.length).toEqual(12)
+		});
+	
+	});
+	
